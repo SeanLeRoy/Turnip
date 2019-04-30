e364693d0b4ed6025df2fb6ff7d615dc60988806
SeanLeRoy <seanme97@gmail.com>
2019-04-30 12:47:46 -0700

    Finish level one and two

---
+++ Turnip/Assets/Scripts/KillOnCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillOnCollision : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Parker" || collision.gameObject.name == "Turnip")
+        {
+            // TODO: Trigger Death Sequence, for now just reset position
+            collision.gameObject.transform.position = new Vector3(0, 0, 0);
+        }
+    }
+}
