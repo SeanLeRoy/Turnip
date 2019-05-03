8713dac3db5fc7a01f26699d935b753113a8fccb
Selene Wartell <selene.wartell@gmail.com>
2019-05-02 19:33:39 -0700

    Resolution manager added, script not connected

---
+++ Turnip/Assets/Scripts/ScreenResolution.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ScreenResolution : MonoBehaviour
+{
+    Dropdown m_Dropdown;
+    public Dropdown Resolution;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(640, 480, true);
+        Debug.Log("testing");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
