fe60fc7e1cd8add7d972dd3909ea968ff45af64a
Selene Wartell <selene.wartell@gmail.com>
2019-05-03 07:03:19 -0700

    Enemy sprite created

---
+++ Turnip/Assets/Scripts/ParkerHealth.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ParkerHealth : MonoBehaviour
+{
+    public int startingHealth = 10;                            // The amount of health Parker starts the level with.
+    public int currentHealth;                                   // The current health Parker has.
+    public Slider healthManager;                                 // Reference to the UI's health bar.
+
+
+    void Awake()
+    {
+        // Set the initial health of the player.
+        currentHealth = startingHealth;
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+
+    public void TakeHit(int damage)
+    {
+        // Reduce the current health by the damage amount.
+        currentHealth -= damage;
+
+        // Set the health bar's value to the current health.
+        healthManager.value = currentHealth;
+
+    }
+}
