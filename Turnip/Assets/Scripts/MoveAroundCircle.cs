0981c1049778f79d6dd10f1cf337925f4d9db43b
SeanLeRoy <seanme97@gmail.com>
2019-05-07 16:44:58 -0700

    Finish fire wheel & ice

---
+++ Turnip/Assets/Scripts/MoveAroundCircle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAroundCircle : MonoBehaviour
+{
+
+    private float RotateSpeed = 5f;
+    private float Radius = 0.1f;
+
+    private Vector2 _centre;
+    private float _angle;
+
+    private void Start()
+    {
+        _centre = transform.position;
+    }
+
+    private void Update()
+    {
+
+        _angle += RotateSpeed * Time.deltaTime;
+
+        var offset = new Vector2(Mathf.Sin(_angle), Mathf.Cos(_angle)) * Radius;
+        transform.position = _centre + offset;
+    }
+
+
+
+}
