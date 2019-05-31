ef7f2b777f8a4c0ec5a5c670707370fe779e7441
Selene Wartell <selene.wartell@gmail.com>
2019-05-31 14:30:10 -0700

    spike fix take 1

---
+++ Turnip/Assets/Scripts/SpikeDamage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeDamage : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        EnemyDamage enemyDamage = new EnemyDamage();
+        enemyDamage.hearts(1);
+    }
+}
