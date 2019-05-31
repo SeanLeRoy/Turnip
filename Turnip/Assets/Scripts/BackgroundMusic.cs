a1015120d684e6db74f309e9bfa68dd140ac528a
Kyle Simpson <kyle3381@gmail.com>
2019-05-31 14:27:54 -0700

    Fix music and pause menu

--- Turnip/Assets/Scripts/BackgroundMusic.cs
+++ Turnip/Assets/Scripts/BackgroundMusic.cs
@@ -4,10 +4,57 @@ using UnityEngine;
 
 public class BackgroundMusic : MonoBehaviour
 {
-    public GameObject backgroundMusic;
+    public static BackgroundMusic instance;
+
+    public AudioSource audioSource;
+
+
+    private bool SongLoaded;
+
+
+    void Awake()
+    {
+        MakeSingleton();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void MakeSingleton()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void PlayMusic(bool play)
+    {
+        if (play)
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            if (audioSource.isPlaying)
+            {
+            }
+            audioSource.Stop();
+        }
+
+    }
+
+
+    //private GameObject backgroundMusic;
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(backgroundMusic);
+        DontDestroyOnLoad(instance);
     }
 }
